a46a05ec789d4ec4014741155915093c554b043a
Jan <Jan@DESKTOP-1QHGUOO>
2021-10-25 14:27:38 +0200

    Přidejte soubory projektu.

---
+++ nejvetsi cislo/nejvetsi cislo.cpp
@@ -0,0 +1,35 @@
+﻿// nejvetsi cislo.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	int a = 1;
+	int b = 9;
+	int c = 18;
+	if (a > b) {
+		;
+		if (a > c) {
+			;
+			printf("cislo %d je nejvetsi", a);
+		}
+		else {
+			if (c > b) {
+				;
+				printf("cislo %d je nejvetsi", c);
+			}
+			else {
+			}
+		}
+	}
+	else {
+		if (b > c) {
+			;
+			printf("cislo %d je nejvetsi", b);
+		}
+		else {
+			printf("cislo %d je nejvetsi", c);
+		}
+	}
+}
